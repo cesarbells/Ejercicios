02e56a522943b26b24a134a6f05cc15151ca7e3a
cesarbells <cesabelsa@gmail.com>
2024-11-23 22:35:24 +0100

    estructura del repositorio

---
+++ Ejercicio2/src/Prsentacion.java
@@ -0,0 +1,7 @@
+public class Prsentacion {
+    public static void main(String[] args) {
+        System.out.println("Hola mi nombre es Cesar");
+        System.out.println("tengo 50 años");
+        System.out.println("Soy español");
+    }
+}
